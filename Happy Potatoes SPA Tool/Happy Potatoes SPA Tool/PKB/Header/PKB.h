486e486cae3e0acab4afdcafafcc2a8cdd941315
feifeiliang <a0125994@u.nus.edu>
2016-02-11 19:35:44 +0800

    add Modifies to PKB.h

--- Happy Potatoes SPA Tool/Happy Potatoes SPA Tool/PKB/Header/PKB.h
+++ Happy Potatoes SPA Tool/Happy Potatoes SPA Tool/PKB/Header/PKB.h
@@ -96,17 +96,48 @@ private:
 
 	// get the child stmt
 	std::vector<string> getChild(STMT stmt);
+
+	// record the parent relationship between two stmtlst
 	void setParent(STMT stmt1, STMT stmt2);
 
 	//FOLLOWS
-
+	// check whether stmt1 is follow stmt2
 	bool isFollow(STMT stmt1, STMT stmt2);
+
+	//set follow relationship between stmt1 and stmt2
 	void setFollow(STMT stmt1, STMT stmt2);
+
+	// return a statmentlist that followed by stmt
 	std::vector<int> getFollower(STMT stmt);
+
+	// return a statmentlist that stmt followed 
 	std::vector<int> getFollow(STMT stmt);
 
 	//CALLS
+	// return the procedure set that called by proc
+	std::vector<string> getCalls(PROC proc);
 
+	// set the call relationship between proc1 and proc2
+	void setCalls(PROC proc1, PROC proc2);
+
+	// return true proc1 calls proc2 otherwise, false
+	bool isCalls(PROC proc1, PROC proc2);
+
+	// Modifies
+
+	// return true is Modifies(var, stmt) otherwise, false
+	bool isModified (VAR var, STMT stmt);
+
+	// return true is Modifies(var, proc) otherwise, false
+	bool isModified (VAR var, PROC proc);
+
+	std::vector<string> getModifies (STMT stmt);
+	std::vector<string> getModifies (PROC proc);
+	std::vector<int> getModifies (VAR var);
+
+
+	//Next
+	//Affect
 
 
 	//VARTABLE
