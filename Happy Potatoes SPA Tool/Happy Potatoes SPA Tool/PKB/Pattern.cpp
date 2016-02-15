761fff43724c6a55830e35e1a557d82e06261eeb
David Chong <zaktansnake@outlook.com>
2016-02-16 00:22:44 +0800

    Updated Pattern.cpp with subtree method
    Updated Pattern.cpp with pseudo code for AST creating for assignment
    statements.

---
+++ Happy Potatoes SPA Tool/Happy Potatoes SPA Tool/PKB/Pattern.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include "./Header/Pattern.h"
+
+using namespace std;
+
+static string temp;
+
+void subTreeCreate(string assignment) {
+
+	//In-order traversal is given by assignment statement
+	//To understand the depth of each node, we can do a left to right reading and put a bracket around trees have a variable and or constant 
+	
+	//In example, we get a string assignment, we need to read it left to right, once we encounter a higher order of operation we bracket the varaibles aroud it
+	//Once completed, method will return the updated string to PKB for storage
+
+}
