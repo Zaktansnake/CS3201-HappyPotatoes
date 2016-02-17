62211fcfd340638251f7da95afb5c4b7e33b243b
feifeiliang <a0125994@u.nus.edu>
2016-02-17 13:57:04 +0800

    edit stmtTable unit test

--- Happy Potatoes SPA Tool/UnitTesting/TestStmtTable.cpp
+++ Happy Potatoes SPA Tool/UnitTesting/TestStmtTable.cpp
@@ -3,17 +3,30 @@
 #include "../Happy Potatoes SPA Tool/PKB/Header/stmtTable.h"
 #include "../Happy Potatoes SPA Tool/PKB/Header/Follows.h"
 #include "../Happy Potatoes SPA Tool/PKB/Header/Parent.h"
+#include <iostream>
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
+// initial
+void setUp() {
+    stmtTable::addStmtTable("i=1", 1);
+
+}
+
 namespace UnitTesting
 {
-	TEST_CLASS(UnitTesting)
+	TEST_CLASS(stmtTable)
 	{
 	public:
-		void testStmtTable() {
-			cout << "Test "
-		}
+		TEST_METHOD(testFollow) {
+			cout << "Test Stmt Follow : ";
+			
 
+		}
+		TEST_METHOD(testParent) {
+			cout << "TestStmtTable::testStmt = ";
+
+		}
 	};
 }
