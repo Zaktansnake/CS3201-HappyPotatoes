230eda40d3605db414701958c8fe88f7a22eb492
Zaktansnake <david_chongym@outlook.com>
2016-02-19 03:35:06 +0800

    Updated TestWrapper.cpp
    Updated TestWrapper.cpp with PQL additions for reference.

--- Happy Potatoes SPA Tool/AutoTester/TestWrapper.cpp
+++ Happy Potatoes SPA Tool/AutoTester/TestWrapper.cpp
@@ -1,5 +1,6 @@
 #include "TestWrapper.h"
 #include "../Happy Potatoes SPA Tool/PKB/Header/Parser.h"
+#include "../Happy Potatoes SPA Tool/PQL/Header/ParseResult.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -27,7 +28,14 @@ void TestWrapper::parse(std::string filename) {
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
 // call your evaluator to evaluate the query here
   // ...code to evaluate query...
-
+	try {
+		PQLParser::parse(query);
+	}
+	catch (exception &e) {
+		cout << e.what() << endl;
+	}
+	vector<std::string> resultVec = QueryEvaluator::evaluate();
+	results = std::list<std::string>(resultVec.begin(), resultVec.end());
   // store the answers to the query in the results list (it is initially empty)
   // each result must be a string.
 }
