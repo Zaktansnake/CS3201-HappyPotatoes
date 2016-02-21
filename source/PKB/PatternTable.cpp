0622822abc63703ee2bd6fc06a086810010bfa76
feifeiliang <a0125994@u.nus.edu>
2016-02-22 02:18:50 +0800

    complete PatternTable

--- source/PKB/PatternTable.cpp
+++ source/PKB/PatternTable.cpp
@@ -4,9 +4,10 @@
 #include <algorithm>
 #include <string>
 #include <vector>
-#include "./Header/PatternTable.h"
-#include "./Header/VarTable.h"
-#include "./Header/Patterns.h"
+#include <iostream>
+#include "Header\PatternTable.h"
+#include "Header\VarTable.h"
+#include "Header\Patterns.h"
 
 
 using namespace std;
@@ -15,46 +16,71 @@ string stmtLine;
 
 // -----------------isPattern--------------------------
 std::vector<int> PatternTable::getVariable(string variable) {
-    return VarTable::getModifiesTable(variable);
+	return VarTable::getModifiesTable(variable);
 }
 string PatternTable::getStatementLine(int stmtNo) {
-    return VarTable::getAssignTable(stmtNo);
+	return VarTable::getAssignTable(stmtNo);
 }
 bool PatternTable::isPattern(string variable, string key) {
-    lineThatModifiedV = getVariable(variable);
+	lineThatModifiedV = getVariable(variable);
 	for (int i = 0; i < lineThatModifiedV.size(); i++) {
-	     stmtLine = getStatementLine(lineThatModifiedV.at(i));
-		 std::size_t found = stmtLine.find(Patterns::patternAssignment(key));
-		 if (found != std::string::npos) {
-			 return true;
-		 }
-		 else {
-			 return false;
-		 }
+		stmtLine = getStatementLine(lineThatModifiedV.at(i));
+		if (stmtLine.find("=") != std::string::npos) {
+			int length = stmtLine.size() - 2;
+			stmtLine = stmtLine.substr(2, length);
+			while (stmtLine.back() == ';' || stmtLine.back() == '}') {
+				stmtLine.pop_back();
+			}
+			stmtLine = Patterns::patternAssignment(stmtLine);
+			std::size_t found = stmtLine.find(Patterns::patternAssignment(key));
+			if (found != std::string::npos) {
+				return true;
+			}
+		}
 	}
+	return false;
 }
-	//----------------------getAssignWithVar---------------------
+//----------------------getAssignWithVar---------------------
 std::vector<int> PatternTable::getAssignWithVar(string variable, string testkey) {
-   std::vector<int> ans;
-	 for (int i = 0; i < lineThatModifiedV.size(); i++) {
-		 stmtLine = getStatementLine(lineThatModifiedV.at(i));
-		 std::size_t found = stmtLine.find(Patterns::patternAssignment(testkey));
-		 if (found != std::string::npos) {
-			 ans.push_back(lineThatModifiedV.at(i));
-		 }
+	std::vector<int> ans;
+	for (int i = 0; i < lineThatModifiedV.size(); i++) {
+		stmtLine = getStatementLine(lineThatModifiedV.at(i));
+		if (stmtLine.find("=") != std::string::npos) {
+			int length = stmtLine.size();
+			length = length - 2;
+			stmtLine.substr(2, length);
+			while (stmtLine.back() == ';' || stmtLine.back() == '}') {
+				stmtLine.pop_back();
+			}
+			stmtLine = Patterns::patternAssignment(stmtLine);
+			std::size_t found = stmtLine.find(Patterns::patternAssignment(testkey));
+			if (found != std::string::npos) {
+				ans.push_back(lineThatModifiedV.at(i));
+			}
+		}
 	}
 	return ans;
 }
 
 //----------------------getAssign---------------------------------
 std::vector<int> PatternTable::getAssign(string testkey) {
-    std::vector<int> ans;
-    lineThatModifiedV = VarTable::getAllAssign();
+	std::vector<int> ans;
+	lineThatModifiedV = VarTable::getAllAssign();
 	for (int i = 0; i < lineThatModifiedV.size(); i++) {
+
 		stmtLine = getStatementLine(lineThatModifiedV.at(i));
-		std::size_t found  = stmtLine.find(Patterns::patternAssignment(testkey));
-		if (found != std::string::npos) {
-		   ans.push_back(lineThatModifiedV.at(i));
+		if (stmtLine.find("=") != std::string::npos) {
+			int length = stmtLine.size();
+			length = length - 2;
+			stmtLine.substr(2, length);
+			while (stmtLine.back() == ';' || stmtLine.back() == '}') {
+				stmtLine.pop_back();
+			}
+			stmtLine = Patterns::patternAssignment(stmtLine);
+			std::size_t found = stmtLine.find(Patterns::patternAssignment(testkey));
+			if (found != std::string::npos) {
+				ans.push_back(lineThatModifiedV.at(i));
+			}
 		}
 	}
 	return ans;
