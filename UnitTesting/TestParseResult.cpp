a269da3a683df33bda8213f21e2b4df9db721e9e
David Chong <zaktansnake@outlook.com>
2016-02-26 03:33:34 +0800

    Added unit testing for ParseResult.cpp
    Added a unit testing cpp for PQL parser component.

---
+++ UnitTesting/TestParseResult.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <iostream>
+#include <string>
+
+#include "../source/PQL/Header/ParseResult.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(TestParseResult)
+	{
+	public:
+		TEST_METHOD(TestP) {
+			string testDeclaration, testQuery;
+
+			// test for generateParseResult
+			testDeclaration = "";
+			testQuery = "";
+			ParseResult expectedResult = ParseResult(); // should construct a object with the expected query tested, need to insert in correct formatted query
+
+			ParseResult p = ParseResult();
+			ParseResult result = p.generateParseResult(testDeclaration, testQuery);
+
+			Assert::AreEqual(result, expectedResult);
+		}
+	};
+}
