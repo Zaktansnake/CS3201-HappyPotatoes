859ed4150e4adc4d873ff001e1ae963967e1a238
David Chong <zaktansnake@outlook.com>
2016-02-25 21:52:07 +0800

    Cleaned up code for PQL Parser
    Cleaned up code for PQL Parser

--- source/PQL/Clause.cpp
+++ source/PQL/Clause.cpp
@@ -1,10 +1,18 @@
 #include "./Header/Clause.h"
 
-ClauseType Clause::getClauseOperation() { return clauseType_; }
-Parameter1 Clause::getFirstParameter() { return parameter1_; }
-Parameter2 Clause::getSecondParameter() { return parameter2_; }
+ClauseType Clause::getClauseOperation() { 
+	return clauseType_; 
+}
+Parameter1 Clause::getFirstParameter() { 
+	return parameter1_; 
+}
+Parameter2 Clause::getSecondParameter() { 
+	return parameter2_; 
+}
 
-Clause::Clause() {}
+Clause::Clause() {
+}
+
 Clause::Clause(ClauseType type, Parameter1 p1, Parameter2 p2) {
 	clauseType_ = type;
 	parameter1_ = p1;
--- source/PQL/Header/Clause.h
+++ source/PQL/Header/Clause.h
@@ -1,17 +1,19 @@
 #pragma once
 #include <string>
-using namespace std;
 
+using namespace std;
 typedef string ClauseType;
 typedef string Parameter1;
 typedef string Parameter2;
 
 class Clause {
 public:
+	// getters
 	ClauseType getClauseOperation();
 	Parameter1 getFirstParameter(); 
 	Parameter2 getSecondParameter();
 
+	// constructors
 	Clause();
 	Clause(ClauseType,Parameter1,Parameter2);
 
