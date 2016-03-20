2f63332624cef7abcc4a984139cc2dd42ddeb3ea
jiaminw12 <jiaminw@ymail.com>
2016-03-20 15:44:08 +0800

    Updated PatternTable functions

--- source/PKB/PatternTable.cpp
+++ source/PKB/PatternTable.cpp
@@ -13,12 +13,29 @@
 
 using namespace std;
 
+map<int, string> AllAssignsTable;
+vector<int> AllAssignLineNum;
 std::vector<int>lineThatModifiedV;
 string stmtLine;
 
-// -----------------isPattern--------------------------
-std::vector<int> PatternTable::getVariable(string variable) {
-	return VarTable::getModifiesTable(variable);
+int checkLocationUnderscore(string line1, string line2);
+
+void PatternTable::setAssignTable() {
+	AllAssignsTable = VarTable::getAssignTable();
+}
+
+void PatternTable::setAssignNum() {
+	AllAssignLineNum = VarTable::getAllAssign();
+}
+
+// get variable from MODIFIES
+std::vector<int> PatternTable::getModifiesVariable(string variable) {
+	return VarTable::getModifiesAssign(variable);
+}
+
+// get variable from USES
+std::vector<int> PatternTable::getUsesVariable(string variable) {
+	return VarTable::getUsesAssig(variable);
 }
 
 string PatternTable::getStatementLine(int stmtNo) {
@@ -26,90 +43,78 @@ string PatternTable::getStatementLine(int stmtNo) {
 }
 
 bool PatternTable::isPattern(string variable, string key) {
-	lineThatModifiedV = getVariable(variable);
-
-	for (int i = 0; i < lineThatModifiedV.size(); i++) {
-		stmtLine = getStatementLine(lineThatModifiedV.at(i));
-
-		if (stmtLine.find("=") != std::string::npos) {
-			int length = stmtLine.size() - 2;
-			stmtLine = stmtLine.substr(2, length);
-
-			while (stmtLine.back() == ';' || stmtLine.back() == '}') {
-				stmtLine.pop_back();
-			}
-
-			stmtLine = Patterns::patternAssignment(stmtLine);
-			std::size_t found = stmtLine.find(Patterns::patternAssignment(key));
-
-			if (found != std::string::npos) {
-				return true;
-			}
-		}
-	}
-
 	return false;
 }
 
-//----------------------getAssignWithVar---------------------
-std::vector<int> PatternTable::getAssignWithVar(string variable, string testkey) {
-	std::vector<int> ans;
+vector<int> PatternTable::getPatternAssignNum(string left, string right) {
+	vector<int> ans;
+	int condition = checkLocationUnderscore(left, right);
 
-	for (int i = 0; i < lineThatModifiedV.size(); i++) {
-		stmtLine = getStatementLine(lineThatModifiedV.at(i));
-
-		if (stmtLine.find("=") != std::string::npos) {
-		    int in = static_cast<int>(stmtLine.find("="));
-			int length = stmtLine.size();
-			string op = stmtLine.substr(0,in);
-
-			if (op.compare(variable) == 0) {
-				length = length - 2;
-				stmtLine.substr(2, length);
-
-				while (stmtLine.back() == ';' || stmtLine.back() == '}') {
-					stmtLine.pop_back();
-				}
-
-				stmtLine = Patterns::patternAssignment(stmtLine);
-				std::size_t found = stmtLine.find(Patterns::patternAssignment(testkey));
-
-				if (found != std::string::npos) {
-					ans.push_back(lineThatModifiedV.at(i));
-				}
-			}
-			
-		}
+	if (condition == 1) {
+		// (_,_)
+		// return all assign num
+	}
+	else if (condition = 2) {
+		// (_,"x+y+1")
+		// Uses -> find out which lines using variable
+		// ans.pushBack(ans)
+	}
+	else if (condition = 3) {
+		// (_,_"x+y+1"_)
+		// Uses -> find out which lines using variable
+		// ans.pushBack(ans)
+	}
+	else if (condition = 4) {
+		// ("x",_"x+y+1"_)
+		// Modifies -> get the variable's stmtNum -> union with assigNum
+		// Uses -> find out which lines using variable
+	}
+	else if (condition = 5) {
+		// ("x","x+y+1")
+		// Modifies -> get the variable's stmtNum -> union with assignNum
+		// Uses -> find out which lines using variable
+	}
+	else if (condition = 6) {
+		// ("x",_)
+		// Modifies -> get the variable's stmtNum -> union with assignNum
 	}
 
 	return ans;
 }
 
-//----------------------getAssign---------------------------------
-std::vector<int> PatternTable::getAssign(string testkey) {
-	std::vector<int> ans;
-	lineThatModifiedV = VarTable::getAllAssign();
-
-	for (int i = 0; i < lineThatModifiedV.size(); i++) {
-		stmtLine = getStatementLine(lineThatModifiedV.at(i));
-
-		if (stmtLine.find("=") != std::string::npos) {
-			int length = stmtLine.size();
-			length = length - 2;
-			stmtLine.substr(2, length);
-
-			while (stmtLine.back() == ';' || stmtLine.back() == '}') {
-				stmtLine.pop_back();
-			}
-
-			stmtLine = Patterns::patternAssignment(stmtLine);
-			std::size_t found = stmtLine.find(Patterns::patternAssignment(testkey));
-
-			if (found != std::string::npos) {
-				ans.push_back(lineThatModifiedV.at(i));
-			}
-		}
-	}
+vector<string> PatternTable::getPatternWithVar(string left, string right) {
+	vector<string> ans;
 
 	return ans;
+}
+
+int checkLocationUnderscore(string line1, string line2) {
+	int result = 0;
+
+	if (line1.compare("_") == 0 && line2.compare("_") == 0) {
+		// (_,_)
+		result = 1;
+	}
+	else if (line1.compare("_") == 0 && line2.substr(0, 1).compare("_") != 0 && line2.substr(line2.size() - 1).compare("_") != 0) {
+		// (_,"x+y+1")
+		result = 2;
+	}
+	else if (line1.compare("_") == 0 && line2.substr(0, 1).compare("_") == 0 && line2.substr(line2.size() - 1).compare("_") == 0) {
+		// (_,_"x+y+1"_)
+		result = 3;
+	}
+	else if (line1.compare("_") != 0 && line2.substr(0, 1).compare("_") == 0 && line2.substr(line2.size() - 1).compare("_") == 0) {
+		// ("x",_"x+y+1"_)
+		result = 4;
+	}
+	else if (line1.compare("_") != 0 && line2.substr(0, 1).compare("_") != 0 && line2.substr(line2.size() - 1).compare("_") != 0) {
+		// ("x","x+y+1")
+		result = 5;
+	}
+	else if (line1.compare("_") != 0 && line2.compare("_") == 0) {
+		// ("x",_)
+		result = 6;
+	}
+
+	return result;
 }
