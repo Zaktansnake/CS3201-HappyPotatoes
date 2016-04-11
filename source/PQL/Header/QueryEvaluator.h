71ea4902b5f8916e7dc6ec9cc898b344c2613431
David Chong <zaktansnake@outlook.com>
2016-04-12 01:43:36 +0800

    Changed QE methods to public
    For unit testing purposes

--- source/PQL/Header/QueryEvaluator.h
+++ source/PQL/Header/QueryEvaluator.h
@@ -9,13 +9,7 @@ public:
 	vector<string> startEvaluator(ParseResult mustPr);
 	bool SelectBool = false;
 	bool NoClause = false;
-	// constructors
-	QueryEvaluator();
-	~QueryEvaluator();
-	vector<string> NoResults;
 
-
-private:
 	bool CheckWith(With with);
 	bool GetAnswerForRightWith(string left, string right);
 	bool GetAnswerForLeftWith(string left, string right);
@@ -31,10 +25,10 @@ private:
 		PatternSet PS, vector<With> WithClauses);
 	bool assessParseResult(ParseResult pr);
 	bool IsSynonym(char c);
-	bool CheckSynonym(string, string,char, char, string);
-	bool GetResultsForBothSynonym(string,string,char,char,string);
+	bool CheckSynonym(string, string, char, char, string);
+	bool GetResultsForBothSynonym(string, string, char, char, string);
 	bool GetResultsForFirstSynonym(string, string, char, char, string);
-	bool GetResultsForSecondSynonym(string, string,char, char, string);
+	bool GetResultsForSecondSynonym(string, string, char, char, string);
 	bool CheckTrueOrFalse(string, string, char, char, string);
 	vector<string> GetAll(char Type);
 	vector<string> GetAllSecondSynonymFromPKB(string P1, string P2, char P1Type, char P2Type, string clausesType);
@@ -42,7 +36,7 @@ private:
 	bool CheckIsResultsFromPkb(string P1, string P2, char P1Type, char P2Type, string clausesType);
 	string GetStringType(char c);
 	bool CheckTempResultSize(vector<string> v);
-	bool GetResultsForNoClause(vector<pair<string,string>>);
+	bool GetResultsForNoClause(vector<pair<string, string>>);
 	vector<string> GetP2Blank(string ClauseType);
 	vector<string> GetP1Blank(string clausesType);
 	bool DoNormalClause(vector<Clause> ClausesVector);
@@ -50,5 +44,14 @@ private:
 	bool ReturnResultsExist(vector<bool> RE);
 	bool DoPatterns(PatternSet);
 	vector<string> CheckPattern(string type, string name, string P1, string p2, string p3);
+
+	// constructors
+	QueryEvaluator();
+	~QueryEvaluator();
+	vector<string> NoResults;
+
+
+private:
+
 };
 
