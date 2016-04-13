ded2d90d207691aa04dd9feed79e761f396571e5
David Han Zhao <David Han Zhao>
2016-04-13 10:47:41 +0800

    fixed checkAndParseDeclaration()

--- source/PQL/ParseResult.cpp
+++ source/PQL/ParseResult.cpp
@@ -161,7 +161,7 @@ bool ParseResult::checkAndParseDeclaration(string declaration, unordered_map<str
 			smatch match = *next;
 			string word = match.str(0);
 			if (word == "stmt" || word == "assign" || word == "while" || word == "variable"
-				|| word == "constant" || word == "prog_line") {
+				|| word == "constant" || word == "prog_line" || word == "procedure" || word == "if") {
 				if (!typeSelected) {
 					type = word;
 					typeSelected = true;
